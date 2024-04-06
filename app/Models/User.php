b63f9f305b41e463e28cbe57cc78ce3914f5a8f9
Pious Sutherland <sutherlandpious@gmail.com>
2024-04-06 22:24:56 +0200

    Remove Plusses from email to prevent reusing the same email for different accounts.

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,6 +35,23 @@ class User extends Authenticatable
         $this->attributes['password'] = bcrypt($password);
     }
 
+    public function setEmailAttribute($email)
+    {
+        // Deconstruct
+        $parts = explode('@', $email);
+    
+        // Find '+'
+        $firstPart = $parts[0];
+        $plusIndex = strpos($firstPart, '+');
+    
+        // Truncate '+' and everything after it in the username
+        if ($plusIndex)
+            $firstPart = substr($firstPart, 0, $plusIndex);
+    
+        // return (new) username@domain
+        return  $firstPart . '@' . (isset($parts[1]) ? $parts[1] : null);
+    }
+    
     public function posts()
     {
         return $this->hasMany(Post::class);
