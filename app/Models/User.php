eced4709a221431b8a6d48b525270ce3fd8eb534
Pious Sutherland <sutherlandpious@gmail.com>
2024-04-09 13:35:17 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,19 +39,19 @@ class User extends Authenticatable
     {
         // Deconstruct
         $parts = explode('@', $email);
-    
+
         // Find '+'
         $firstPart = $parts[0];
         $plusIndex = strpos($firstPart, '+');
-    
+
         // Truncate '+' and everything after it in the username
         if ($plusIndex)
             $firstPart = substr($firstPart, 0, $plusIndex);
-    
+
         // return (new) username@domain
-        return  $firstPart . '@' . (isset($parts[1]) ? $parts[1] : null);
+        $this->email = $firstPart . '@' . (isset($parts[1]) ? $parts[1] : null);
     }
-    
+
     public function posts()
     {
         return $this->hasMany(Post::class);
